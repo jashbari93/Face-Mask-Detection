d2ff93f1c0eb225cc2c71df54c07d2b0c4a63c88
jashbari93 <73014724+jashbari93@users.noreply.github.com>
2022-08-18 15:51:11 +0530

    Add files via upload

---
+++ FaceDetectionDeploy/modelload.py
@@ -0,0 +1,307 @@
+# USAGE
+# python modelload.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+# C:\Users\Jash Bari\OneDrive\Pictures\Camera Roll\WIN_20210815_16_30_41_Pro
+# import the necessary packages
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+import os
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets.utils import download_url
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+from torchvision.utils import make_grid
+import matplotlib
+import matplotlib.pyplot as plt
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+import skimage.transform as tf
+from torchvision import models
+import torchvision.transforms.functional as TF
+from tqdm.notebook import tqdm
+
+##-------------------------Use GPU if Available------------------------------------
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+
+##-------------------------Model Classes and Fucntions------------------------------------
+
+'''Already ran various models and hyperparameter tuning done on colab, here just 
+initializing model and using saved state dictionary of model and running it'''
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+class FaceMask(ImageClassificationBase):
+    def __init__(self, num_classes, pretrained=True):
+        super().__init__()
+        # Use a pretrained model
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.mobilenet_v2(pretrained=pretrained) #pretrained inherits the weights when it was trained
+        # Replace last layer
+        #self.network.fc = nn.Linear(self.network.fc.in_features, num_classes)
+        self.network.classifier[1] = nn.Linear(1280,2)
+
+        for param in self.network.parameters():
+            param.require_grad = False
+        for param in self.network.classifier[1].parameters():
+            param.require_grad = True
+
+    def forward(self, xb):
+        return self.network(xb)
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        for batch in tqdm(train_loader):
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader,
+                  weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD):
+    torch.cuda.empty_cache()
+    history = []
+
+    # Set up custom optimizer with weight decay
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs,
+                                                steps_per_epoch=len(train_loader))
+
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in tqdm(train_loader):
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+
+            # Gradient clipping
+            if grad_clip:
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Record & update learning rate
+            lrs.append(get_lr(optimizer))
+            sched.step()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+model=FaceMask(2)
+##-------------------------Loading model state dictionary------------------------------------
+
+classes=['mask','no mask']
+
+model.load_state_dict(torch.load(r'C:\Users\Jash Bari\Downloads\Models\model3.pth', map_location='cpu'))
+model.eval()
+
+##-------------------------Predict Functions------------------------------------
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    _, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return classes[preds[0].item()]
+
+##-------------------------FaceMask detection model------------------------------------
+
+'''Used open source OpenCV DNN face detection model and tuned the code
+using face mask classifer to output red frame for no mask and green frame for mask'''
+
+# construct the argument parse and parse the arguments
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+                help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+                help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.3,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized model from disk
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# initialize the video stream and allow the cammera sensor to warmup
+
+#print("[INFO] starting video stream...")
+#vs = VideoStream(src=0).start()
+#time.sleep(2.0)
+
+# loop over the frames from the video stream
+# grab the frame from the threaded video stream and resize it
+# to have a maximum width of 400 pixels
+frame=cv2.imread()
+frame = imutils.resize(frame)
+
+# grab the frame dimensions and convert it to a blob
+(h, w) = frame.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                             (300, 300), (104.0, 177.0, 123.0))     # Normalizes the image
+
+# pass the blob through the network and obtain the detections and
+# predictions
+net.setInput(blob)
+detections = net.forward()
+
+# loop over the detections
+for i in range(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with the
+    # prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections by ensuring the `confidence` is
+    # greater than the minimum confidence
+    if confidence < args["confidence"]:
+        continue
+
+    # compute the (x, y)-coordinates of the bounding box for the
+    # object
+    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+
+    # draw the bounding box of the face along with the associated
+    # probability
+    text = "{:.2f}%".format(confidence * 100)
+    y = startY - 10 if startY - 10 > 10 else startY + 10
+    frame2=frame.copy()
+    face= frame2[startY-30:endY+30,startX-30:endX+30]
+    face2=face.copy()
+
+    # if Face is detected, passed the cropped face from frame in trained mask
+    # classifier and output the green(masked) red(not masked) frame
+    try:
+        face
+    except NameError:
+        pass
+    else:
+        #face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (128, 128))
+        face = TF.to_pil_image(face)
+        face = TF.to_grayscale(face, num_output_channels=3)
+        face = TF.to_tensor(face)
+        if predict_image(face, model) == 'mask':
+            cv2.rectangle(frame, (startX, startY), (endX, endY),  # green
+                          (0, 255, 0), 2)
+            cv2.putText(frame, text, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+        else:
+            cv2.rectangle(frame, (startX, startY), (endX, endY),  # green
+                          (0, 0, 255), 2)
+            cv2.putText(frame, text, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+# show the output frame
+cv2.imshow("Frame", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
